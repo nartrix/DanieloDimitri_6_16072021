e6c6d3aeb568f2afb19a15cfce733cb03ba813c5
danielo <dimitri.danielo@laposte.net>
2021-08-02 20:57:02 +0200

    feat: add packages helmet, password-validator and cors

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,10 +3,10 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    const userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
-      throw 'Invalid user ID';
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); // check the token
+    const userId = decodedToken.userId; 
+    if (req.body.userId && req.body.userId !== userId) { // checks the user and his token and if it does not match is redirected to the error
+      throw 'Invalid user ID'; 
     } else {
       next();
     }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const passwordValidator = require('../middlewares/password-validator')
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', passwordValidator, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
